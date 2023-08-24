a7fa64dd327c2fbb40a36c01722cafaa529d5048
AQIL21B6005 <21b6005@ubd.edu.bn>
2023-08-24 10:07:41 +0800

    First commit (sq, merge, tups)

---
+++ even_square_list.py
@@ -0,0 +1,5 @@
+input = [1,2,3,4,5,6]
+
+for i in input:
+    if i % 2 == 0:
+        print(i*i)
---
+++ merge_dicts.py
@@ -0,0 +1,6 @@
+dict_1 = {1: "A", 2: "B"}
+dict_2 = {2: "F", 4: "C"}
+
+dict_1.update(dict_2)
+
+print(dict_1)
