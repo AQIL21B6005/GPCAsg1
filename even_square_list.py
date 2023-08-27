c3cda2d23d92ab8f81d80c63bc782953d33b90d0
AQIL21B6005 <21b6005@ubd.edu.bn>
2023-08-27 17:53:26 +0800

    Add user input for even square list

--- even_square_list.py
+++ even_square_list.py
@@ -1,5 +1,11 @@
-input = [1,2,3,4,5,6]
+# input = [1,2,3,4,5,6]
 
-for i in input:
+# User input
+str_input = input("Enter a list of numbers separated by spaces: ")
+input_lst = [int(x) for x in str_input.split()]
+
+
+# change input_lsts to input if using preset string
+for i in input_lst:
     if i % 2 == 0:
         print(i*i)
